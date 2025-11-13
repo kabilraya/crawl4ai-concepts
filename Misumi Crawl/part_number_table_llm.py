0b548058d3347bfede7d46c83f2d10922190ac7c
kabilraya <roykabil99@gmail.com>
2025-11-13 16:50:19 +0545

    pagination implemented successfully

---
+++ Misumi Crawl/part_number_table_llm.py
@@ -0,0 +1,195 @@
+from crawl4ai import AsyncWebCrawler, CacheMode, BrowserConfig, CrawlerRunConfig,LLMConfig
+from crawl4ai.markdown_generation_strategy import DefaultMarkdownGenerator
+from crawl4ai.extraction_strategy import JsonCssExtractionStrategy
+import asyncio
+import uuid
+import json
+import pandas as pd
+from dotenv import load_dotenv
+import os
+from bs4 import BeautifulSoup
+import re
+from internal_links_extraction import product_links_extraction
+
+"""
+pagination css_selector = div.Pagination_container__VOa4I>li:nth-child(2)>a
+"""
+
+async def json_css_product_extraction():
+    
+    # internal_links =await  product_links_extraction()
+    all_data = []
+    load_dotenv()
+    base_url = "https://vn.misumi-ec.com/vona2/detail/110302555550/?list=PageCategory"
+    api_key = os.getenv("GOOGLE_API_KEY")  
+    session_id = str(uuid.uuid4())
+    query = "Generate a schema to extract product details.The column names from the tables MUST exact. Only use the table column names as the key_fields in the json schema(Split the Part Number header into two keys - Part Number Name and Part Number URL)  You MUST use CSS selectors, not XPath. The base selector MUST ONLY  be 'div.mergedRow'."
+    initial_run_config = CrawlerRunConfig(
+            delay_before_return_html=40.0,
+            session_id=session_id,
+            js_code="""
+                return new Promise((resolve)=>{
+                setTimeout(()=>{
+                const langButton = document.querySelector('a[lang="en"]');
+                if (langButton) langButton.click();
+                },3000)
+                
+                setTimeout(() => {
+                const partTab = document.querySelector('a#codeList');
+                if (partTab) partTab.click();
+                
+            setTimeout(()=>{
+            const partRows = document.querySelectorAll('tr.PartNumberColumn_dataRow__43D6Y');
+            const priceRows = document.querySelectorAll('tr.PartNumberAsideColumns_dataRow__OUw8N');
+            const specRows = document.querySelectorAll('tr.PartNumberSpecColumns_dataRow__M4B4a');
+
+            
+            
+            const mainContainer = document.querySelector('div.PartNumberList_mainOuter__d74Qg');
+            const mergedContainer = document.createElement('div');
+            mergedContainer.classList.add('mergedRowContainer');
+            const numRows = Math.min(partRows.length, priceRows.length, specRows.length);
+            for (let i = 0; i < numRows; i++) {
+            const row = document.createElement('div');
+            row.classList.add('mergedRow');
+            row.appendChild(partRows[i]);
+            row.appendChild(priceRows[i]);
+            row.appendChild(specRows[i]);
+            mainContainer.appendChild(row)
+            }
+            resolve(true);
+            },10000);
+            }, 20000);
+            });
+            """,
+            wait_for = "css:div#deTabsContainer"
+            
+        )
+    # initial_result = await crawler.arun(url="https://vn.misumi-ec.com/vona2/detail/221006228542?list=PageCategory", config=initial_run_config
+    
+    browser_config = BrowserConfig(headless = False, verbose = True, enable_stealth=True)
+    sub_category = ""
+    async with AsyncWebCrawler(config = browser_config) as crawler:
+        # for link in internal_links:
+
+            initial_result = await crawler.arun(url=base_url, config=initial_run_config)        
+            soup = BeautifulSoup(initial_result.html, "html.parser")
+        
+            table_container = soup.select_one("div.PartNumberList_mainOuter__d74Qg")
+            title_element = soup.select_one("h1.PageHeading_wrap__K1c1n")
+            sub_category = title_element.get_text(strip=True) if title_element else "unknown_product" 
+            cleaned_html = str(table_container)
+            css_schema = JsonCssExtractionStrategy.generate_schema(
+            html=cleaned_html,
+            schema_type="css", 
+            query=query,
+            llm_config = LLMConfig(provider="gemini/gemini-2.5-flash",api_token=api_key)
+        )
+            print(css_schema)
+            for i in range(1,3):
+                if i == 1:
+                    run_config = CrawlerRunConfig(
+                    word_count_threshold=10,
+                exclude_all_images=True,
+                exclude_external_links=True,
+                css_selector="h1.PageHeading_wrap__K1c1n,p.BasicInformation_catchCopy__h8mKx,div.PartNumberList_mainOuter__d74Qg",
+                # process_iframes=True,
+                cache_mode=CacheMode.DISABLED,
+                exclude_domains=[],
+                exclude_social_media_domains=[],
+                exclude_social_media_links=True,
+                mean_delay=2.0,
+                delay_before_return_html=20.0,
+                extraction_strategy=JsonCssExtractionStrategy(schema=css_schema),
+                markdown_generator=DefaultMarkdownGenerator(),
+
+                js_only=True,
+                session_id=session_id,
+                excluded_tags=["footer"],
+                remove_forms=True,
+                remove_overlay_elements=True
+        )
+                    result = await crawler.arun(url = base_url,config = run_config)
+                    data = json.loads(result.extracted_content)
+                    print(len(data))
+                    all_data.extend(data)
+                else:
+                    
+                    run_config = CrawlerRunConfig(
+                    word_count_threshold=10,
+                exclude_all_images=True,
+                exclude_external_links=True,
+                css_selector="h1.PageHeading_wrap__K1c1n,p.BasicInformation_catchCopy__h8mKx,div.PartNumberList_mainOuter__d74Qg",
+                # process_iframes=True,
+                cache_mode=CacheMode.DISABLED,
+                exclude_domains=[],
+                exclude_social_media_domains=[],
+                exclude_social_media_links=True,
+                mean_delay=2.0,
+                delay_before_return_html=60.0,
+                extraction_strategy=JsonCssExtractionStrategy(schema=css_schema),
+                markdown_generator=DefaultMarkdownGenerator(),
+                
+                js_code="""
+                            return new Promise((resolve)=>{
+                            setTimeout(()=>{
+                    document.querySelector('a#codeList').click();
+                    
+                    setTimeout(() => {
+                    
+                    
+                    const nextButton = document.querySelector('div.Pagination_container__VOa4I:nth-of-type(2) > li:nth-child(2) > a');
+                    if(nextButton){
+                        nextButton.click();
+                    
+                    setTimeout(()=>{
+                            const mainContainer = document.querySelector('div.PartNumberList_mainOuter__d74Qg');
+                            
+
+                            const existingRows = mainContainer.querySelectorAll('div.mergedRow');
+                            existingRows.forEach(r => r.remove());
+                            
+                            
+                                // Step 4: Run the row-merging logic on the NEW data for page 2.
+                                const partRows = document.querySelectorAll('tr.PartNumberColumn_dataRow__43D6Y');
+                                const priceRows = document.querySelectorAll('tr.PartNumberAsideColumns_dataRow__OUw8N');
+                                const specRows = document.querySelectorAll('tr.PartNumberSpecColumns_dataRow__M4B4a');
+                                const numRows = Math.min(partRows.length, priceRows.length, specRows.length);
+                                
+
+                                for (let j = 0; j < numRows; j++) {
+                                    const row = document.createElement('div');
+                                    row.classList.add('mergedRow');
+                                    row.appendChild(partRows[j]);
+                                    row.appendChild(priceRows[j]);
+                                    row.appendChild(specRows[j]);
+                                    mainContainer.appendChild(row);
+                                }
+                            },20000);
+                            }
+                            resolve(true);
+                            },3000);
+                            },5000);
+                            });
+                        """,
+                        js_only=True,
+
+                        session_id=session_id,
+                        excluded_tags=["footer"],
+                        remove_forms=True,
+                        remove_overlay_elements=True
+        )
+                    result = await crawler.arun(url = base_url,config = run_config)
+
+        
+                    data = json.loads(result.extracted_content)
+                    print(len(data))
+                    all_data.extend(data)
+            
+    print(len(all_data))
+    df = pd.DataFrame(all_data)
+    sub_category = re.sub(r'[ ,]+', '_', sub_category)
+    file_name = f"camfollower_{sub_category}.xlsx".lower()
+        
+    df.to_excel(file_name, index=False)
+asyncio.run(json_css_product_extraction())
